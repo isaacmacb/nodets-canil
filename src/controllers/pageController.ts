d450204e5167236c0a33cfd2c2ee573d91af52b9
isaacmacb <liberdadebessa@gmail.com>
2023-07-10 00:44:25 -0300

    panels e models

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,22 +1,23 @@
 import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
-        res.send('home no controller')
-        //res.render('pages/page')
+    res.render('pages/page')
 
 }
 
 export const dogs = (req: Request, res: Response) => {
-    //res.render('pages/page')
+    res.send('dogs')
 
 }
 export const cats = (req: Request, res: Response) => {
 
-    //res.render('pages/page')
+    res.send('cats')
+
 
 }
 export const fishes = (req: Request, res: Response) => {
 
-    //res.render('pages/page')
+    res.send('fishes')
+
 
 }
